d4659f1252426c3879a3cefef60c82605177b393
QuentinDST <83666778+QuentinDST@users.noreply.github.com>
2023-02-13 14:10:32 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * DEV WEB MOBILE Portfolio
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage WP Portfolio
+ */
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header()?>
+
+    ici index
+
+<?php get_footer()?>
